fa8ea579856b1bfb5723428654b371bf8b28eda1
ruden <ruden91@naver.com>
2018-07-15 14:01:16 +0900

    contentful test

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -1,8 +1,11 @@
 const contentful = require("contentful");
 console.log(process.env);
 const client = contentful.createClient({
-  space: process.env.CTF_SPACE_ID,
-  accessToken: process.env.CTF_ACCESS_TOKEN
+  space: "hu4faubeotg0",
+  accessToken:
+    "98beb767cc8236512e0585eb3063ccc48d6e69894845851743a3f60e2a60b358"
+  // space: process.env.CTF_SPACE_ID,
+  // accessToken: process.env.CTF_ACCESS_TOKEN
 });
 
 module.exports = client;
