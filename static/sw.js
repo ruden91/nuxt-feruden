dd4454dc8d69c4cb599b70870984a2fd9a7ccfe2
ruden <ruden91@mycelebs.com>
2019-05-09 13:40:01 +0900

    update

--- static/sw.js
+++ static/sw.js
@@ -8,40 +8,40 @@ const workboxSW = new self.WorkboxSW({
 
 workboxSW.precache([
   {
-    "url": "/_nuxt/app.04ba5cf9124b304bae3d.js",
-    "revision": "d1da2ed9da88718ad41d7822a1404c59"
+    "url": "/_nuxt/app.d8e16b5b57450a5257cf.js",
+    "revision": "2e8b6f7a21267b8884764df365defa4f"
   },
   {
-    "url": "/_nuxt/layouts/blog.225765a9758cb105dc28.js",
-    "revision": "a6e0641907f9cd10d53abd64abae892e"
+    "url": "/_nuxt/layouts/blog.bfb4bd64a9f212b95e28.js",
+    "revision": "9ba14f5e7f26e854e150ef0378aab429"
   },
   {
-    "url": "/_nuxt/layouts/default.74cdef2ce8d42cfb75b2.js",
-    "revision": "48a69cd8c732fc65e45efec40451a875"
+    "url": "/_nuxt/layouts/default.75ddf4563ead26cf0c81.js",
+    "revision": "8187733d9b150811e664f78bad943ca6"
   },
   {
-    "url": "/_nuxt/layouts/test.9aadd49a93cf26808154.js",
-    "revision": "d61a35bfff40dbaab0fa2bf862474052"
+    "url": "/_nuxt/layouts/test.a056c29f35af9ca039da.js",
+    "revision": "12c12d8b763f0739862c51f615a787f9"
   },
   {
-    "url": "/_nuxt/manifest.47c7dba38ae4ec1b4f45.js",
-    "revision": "d9ee9572101df0118494951cdaf13743"
+    "url": "/_nuxt/manifest.9a8e39af5c88bb3c3a85.js",
+    "revision": "a6aa77c4f1bb72fb308f0133a28878f5"
   },
   {
-    "url": "/_nuxt/pages/blog/_slug.2ec79733272c719d42ad.js",
-    "revision": "cbf2b875eb173f96eae845af68069f0e"
+    "url": "/_nuxt/pages/blog/_slug.bfafab025eb099e40e67.js",
+    "revision": "d2eece805177c4be8f7486d3f085ae5c"
   },
   {
-    "url": "/_nuxt/pages/index.23a3b2d314dae28d131c.js",
-    "revision": "6f6c87e41bc2d00a23a2b0afc26589cb"
+    "url": "/_nuxt/pages/index.33a89e273d8136ece247.js",
+    "revision": "6c7aa9b388363be7d3e4d35adc305356"
   },
   {
-    "url": "/_nuxt/pages/tags/_tag.7d88b770ae2fd2e49781.js",
-    "revision": "db7392cc16f74b7fe2348caca13a3ba3"
+    "url": "/_nuxt/pages/tags/_tag.c27c0b617f2dee80f94d.js",
+    "revision": "3863248f574cf32f5fe60ad628a063d0"
   },
   {
-    "url": "/_nuxt/pages/test/index.63bdd55edc6c57394b90.js",
-    "revision": "3333632bf54dd60421d474598b090546"
+    "url": "/_nuxt/pages/test/index.99f740951d175b2b3936.js",
+    "revision": "7c4b0763b2b5440796b8ac8e5f0e9d2f"
   },
   {
     "url": "/_nuxt/vendor.0d2404c6592b2af2f6d7.js",
